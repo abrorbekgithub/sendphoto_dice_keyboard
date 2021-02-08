ccab0be96e6e383fd0f30702fcd8ddb58460fc42
abrorbek <abrorbekgithub@gmail.com>
2021-02-08 16:39:53 +0500

    emoji ni sinab korildi ishlamadi

--- sendDice.py
+++ sendDice.py
@@ -4,8 +4,6 @@ from pprint import pprint
 import os
 
 #1091584118
-#'https://random.dog/85ca20a7-e792-4166-8709-1e0710b6d68d.jpg'
-#'https://random.dog/53549b25-0201-4d08-a792-f05f0c78f1fd.mp4'
 
 token=os.environ["TOKEN"]
 ids=1091584118
@@ -13,11 +11,9 @@ ids=1091584118
 
 def sendDice(ids):
     url=f'https://api.telegram.org/bot{token}/sendDice'
-    # photo='https://random.dog/85ca20a7-e792-4166-8709-1e0710b6d68d.jpg'
     p={
         "chat_id":ids,
-        # "photo":photo,
-        # "caption":"this is dog"
+        "emoji":"25"
     }
     res=requests.get(url,params=p)
     return res.json()
