0e860ac8d0f9433d3331c0a0a40a09d3d59dcbdb
abrorbek <abrorbekgithub@gmail.com>
2021-02-09 09:43:07 +0500

    pro buttons

---
+++ pro.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from pprint import pprint
+import os
+
+#1091584118
+
+token=os.environ["TOKEN"]
+ids=1091584118
+
+
+def reply_markup(ids):
+    url=f'https://api.telegram.org/bot{token}/sendMessage'
+    p={
+        "chat_id":ids,
+        "text":"PRO",
+        "reply_markup":{
+            "keyboard":[ [{"text":"PRO"}], [{"text":"Mashqlar"},{"text":"So'zlar"},{"text":"Reyting"}],
+            [{"text":"Bellashuv"},{"text":"Qo'llanma"}],[{"text":"Sozlamalar"}] ],
+            "resize_keyboard":True
+        }
+    }
+    res=requests.post(url,json=p)
+    return res.json()
+
+pprint(reply_markup(ids))
