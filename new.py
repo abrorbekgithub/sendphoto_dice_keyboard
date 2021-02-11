245c6a9e1b6a1f83b1852c4c4f675b0e2503a7e9
abrorbek <abrorbekgithub@gmail.com>
2021-02-11 11:25:39 +0500

    2.create getUpdates func

--- new.py
+++ new.py
@@ -3,5 +3,11 @@ import json
 import os
 from pprint import pprint
 
-token=os.environ["DOGTOKEN"]
-pprint(token)
+token=os.environ["TOKEN"]
+
+def getUpdates():
+    url=f'https://api.telegram.org/bot{token}/getUpdates'
+    res=requests.get(url=url)
+    update=res.json()["result"]
+    return update
+
